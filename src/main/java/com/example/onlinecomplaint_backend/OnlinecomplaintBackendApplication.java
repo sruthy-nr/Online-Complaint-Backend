14ea7dc90066731cc137cff6203cb2958b7d5872
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-09 14:32:01 +0530

    master

---
+++ src/main/java/com/example/onlinecomplaint_backend/OnlinecomplaintBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinecomplaint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecomplaintBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecomplaintBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinecomplaint_backend/OnlinecomplaintBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinecomplaint_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecomplaintBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
